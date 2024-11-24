600c0b81c788809cd318670e3b5d503656868a08
secretwebmaster <earnbyshare2016@gmail.com>
2024-11-24 19:24:46 +0800

    Add new controllers for payment gateways, prices, and orders

---
+++ src/Http/Controllers/Backend/PaymentGatewayController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Wncms\Http\Controllers\Backend;
+
+use Wncms\Models\PaymentGateway;
+use Illuminate\Http\Request;
+
+class PaymentGatewayController extends BackendController
+{
+    public function index(Request $request)
+    {
+        $q = PaymentGateway::query();
+
+        $paymentGateways = $q->paginate($request->page_size ?? 100);
+
+        return view('wncms::backend.payment_gateways.index', [
+            'page_title' =>  wncms_model_word('payment_gateway', 'management'),
+            'payment_gateways' => $paymentGateways,
+            'statuses' => PaymentGateway::STATUSES,
+        ]);
+    }
+
+    public function create(PaymentGateway $paymentGateway = null)
+    {
+        $paymentGateway ??= new PaymentGateway;
+
+        return view('wncms::backend.payment_gateways.create', [
+            'page_title' =>  wncms_model_word('payment_gateway', 'management'),
+            'paymentGateway' => $paymentGateway,
+            'statuses' => PaymentGateway::STATUSES,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        // dd($request->all());
+
+        $paymentGateway = PaymentGateway::create([
+            'name' => $request->name,
+            'status' => $request->status ?? 'active',
+            'slug' => $request->slug,
+            'type' => $request->type,
+            'account_id' => $request->account_id,
+            'client_id' => $request->client_id,
+            'client_secret' => $request->client_secret,
+            'endpoint' => $request->endpoint,
+            'attributes' => $request->attributes ?? [],
+            'description' => $request->description,
+        ]);
+
+        wncms()->cache()->flush(['payment_gateways']);
+
+        return redirect()->route('payment_gateways.edit', [
+            'paymentGateway' => $paymentGateway,
+        ])->withMessage(__('wncms::word.successfully_created'));
+    }
+
+    public function edit(PaymentGateway $paymentGateway)
+    {
+        return view('wncms::backend.payment_gateways.edit', [
+            'page_title' =>  wncms_model_word('payment_gateway', 'management'),
+            'paymentGateway' => $paymentGateway,
+            'statuses' => PaymentGateway::STATUSES,
+        ]);
+    }
+
+    public function update(Request $request, PaymentGateway $paymentGateway)
+    {
+        $attributes = collect($request->input('attributes', []))
+        ->filter(fn($attr) => isset($attr['key'], $attr['value']) && $attr['key'] !== null && $attr['value'] !== null)
+        ->values() // Reset array keys
+        ->toArray();
+
+        $paymentGateway->update([
+            'name' => $request->name,
+            'status' => $request->status ?? 'active',
+            'slug' => $request->slug,
+            'type' => $request->type,
+            'account_id' => $request->account_id,
+            'client_id' => $request->client_id,
+            'client_secret' => $request->client_secret,
+            'endpoint' => $request->endpoint,
+            'attributes' => $attributes,
+            'description' => $request->description,
+        ]);
+
+        wncms()->cache()->flush(['payment_gateways']);
+
+        return redirect()->route('payment_gateways.edit', [
+            'paymentGateway' => $paymentGateway,
+        ])->withMessage(__('wncms::word.successfully_updated'));
+    }
+
+
+    public function destroy(PaymentGateway $paymentGateway)
+    {
+        $paymentGateway->delete();
+        return redirect()->route('payment_gateways.index')->withMessage(__('wncms::word.successfully_deleted'));
+    }
+
+    public function bulk_delete(Request $request)
+    {
+        if (!is_array($request->model_ids)) {
+            $modelIds = explode(",", $request->model_ids);
+        } else {
+            $modelIds = $request->model_ids;
+        }
+
+        $count = PaymentGateway::whereIn('id', $modelIds)->delete();
+
+        if ($request->ajax()) {
+            return response()->json([
+                'status' => 'success',
+                'message' => __('wncms::word.successfully_deleted_count', ['count' => $count]),
+            ]);
+        }
+
+        return redirect()->route('payment_gateways.index')->withMessage(__('wncms::word.successfully_deleted_count', ['count' => $count]));
+    }
+}
---
+++ src/Http/Controllers/Backend/PriceController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Price;
+use Illuminate\Http\Request;
+
+class PriceController extends Controller
+{
+    public function index(Request $request)
+    {
+        $q = Price::query();
+        
+        $Prices = $q->paginate($request->page_size ?? 100);
+
+        return view('backend.prices.index', [
+            'page_title' =>  wncms_model_word('price', 'management'),
+            'Prices' => $Prices,
+        ]);
+    }
+
+    public function create(Price $Price = null)
+    {
+        $Price ??= new Price;
+
+        return view('backend.prices.create', [
+            'page_title' =>  wncms_model_word('price', 'management'),
+            'Price' => $Price,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+
+        $Price = Price::create([
+            'xxxx' => $request->xxxx,
+        ]);
+
+        wncms()->cache()->flush(['Prices']);
+
+        return redirect()->route('prices.edit', [
+            'Price' => $Price,
+        ])->withMessage(__('wncms::word.successfully_created'));
+    }
+
+    public function edit(Price $Price)
+    {
+        return view('backend.prices.edit', [
+            'page_title' =>  wncms_model_word('price', 'management'),
+            'Price' => $Price,
+        ]);
+    }
+
+    public function update(Request $request, Price $Price)
+    {
+        dd($request->all());
+
+        $Price->update([
+            'xxxx' => $request->xxxx,
+        ]);
+
+        wncms()->cache()->flush(['Prices']);
+        
+        return redirect()->route('prices.edit', [
+            'Price' => $Price,
+        ])->withMessage(__('wncms::word.successfully_updated'));
+    }
+
+    public function destroy(Price $Price)
+    {
+        $Price->delete();
+        return redirect()->route('prices.index')->withMessage(__('wncms::word.successfully_deleted'));
+    }
+
+    public function bulk_delete(Request $request)
+    {
+        if(!is_array($request->model_ids)){
+            $modelIds = explode(",", $request->model_ids);
+        }else{
+            $modelIds = $request->model_ids;
+        }
+
+        $count = Price::whereIn('id', $modelIds)->delete();
+
+        if($request->ajax()){
+            return response()->json([
+                'status' => 'success',
+                'message' => __('wncms::word.successfully_deleted_count', ['count' => $count]),
+            ]);
+        }
+
+        return redirect()->route('prices.index')->withMessage(__('wncms::word.successfully_deleted_count', ['count' => $count]));
+    }
+}
