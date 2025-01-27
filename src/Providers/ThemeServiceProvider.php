518bd8d05d37d9b9342a091d693d3d9556fbcf50
secretwebmaster <earnbyshare2016@gmail.com>
2025-01-28 03:05:59 +0800

    Add ThemeServiceProvider

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wncms\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // handle theme functions
+    }
+}
