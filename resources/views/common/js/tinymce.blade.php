e27002ee640c6cf27d41ccecc6ef691af92b599e
secretwebmaster <earnbyshare2016@gmail.com>
2025-02-04 15:11:05 +0800

    Code clean up

--- resources/views/common/js/tinymce.blade.php
+++ resources/views/common/js/tinymce.blade.php
@@ -1,32 +1,29 @@
 <script>
+    /**
+     * Supported Tool
+     * 
+     * cut
+     * copy
+     * paste
+     * 
+     * tabledelete
+     * tableprops
+     * tablerowprops
+     * tablecellprops
+     * tableinsertrowbefore
+     * tableinsertrowafter
+     * tabledeleterow
+     * tableinsertcolbefore
+     * tableinsertcolafter
+     * tabledeletecol
+    */
     var options = {
         selector: "{{ $editorSelector ?? '#kt_docs_tinymce_basic' }}", 
         height:1000,
         menubar: true,
         promotion: false,
         language: 'zh_TW',
-        toolbar: [
-            "styles fontsize fontsizeinput forecolor backcolor bold italic underline lineheight alignleft aligncenter alignright alignjustify wordcount accordion anchor undo redo link unlink bullist numlist outdent indent blockquote image emoticons fullscreen insertdatetime searchreplace table code"
-            /**
-             * Supported Tool
-             * 
-             * cut
-             * copy
-             * paste
-             * 
-             * tabledelete
-             * tableprops
-             * tablerowprops
-             * tablecellprops
-             * tableinsertrowbefore
-             * tableinsertrowafter
-             * tabledeleterow
-             * tableinsertcolbefore
-             * tableinsertcolafter
-             * tabledeletecol
-             */
-
-        ],
+        toolbar: ["styles fontsize fontsizeinput forecolor backcolor bold italic underline lineheight alignleft aligncenter alignright alignjustify wordcount accordion anchor undo redo link unlink bullist numlist outdent indent blockquote image emoticons fullscreen insertdatetime searchreplace table code"],
         plugins : "lists advlist code image table wordcount link emoticons fullscreen insertdatetime searchreplace accordion anchor",
         images_file_types: 'jpg,svg,webp,png,gif',
         images_upload_url: '{{ route("uploads.image") }}',
