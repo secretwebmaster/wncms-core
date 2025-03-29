713ea95169ab7c53144e9da075f561de6285eb15
secretwebmaster <earnbyshare2016@gmail.com>
2025-03-30 05:04:13 +0800

    Update backend index pages status and boolean field style

--- resources/views/common/table_is_active.blade.php
+++ resources/views/common/table_is_active.blade.php
@@ -1,5 +1,5 @@
 @if(((object)$model)->{$active_column ?? 'is_active'} ?? false)
-<span class="badge badge-success">@lang('wncms::word.yes')</span>
+<span class="@if(!empty($badgeStyle)) badge badge-success @else text-success fw-bold @endif">@lang('wncms::word.yes')</span>
 @else
-<span class="badge badge-danger">@lang('wncms::word.no')</span>
+<span class="@if(!empty($badgeStyle)) badge badge-danger @else text-danger fw-bold @endif">@lang('wncms::word.no')</span>
 @endif
--- resources/views/common/table_status.blade.php
+++ resources/views/common/table_status.blade.php
@@ -1,16 +1,25 @@
-@if(((object)$model)->{$status_attribute ?? 'status'} == ($status_active ?? 'active') || in_array(((object)$model)->{$status_attribute ?? 'status'}, array_merge(['active', 'paid', 'success'], $active_statuses ?? [])))
-<span class="badge badge-success">@lang('wncms::word.' . ((object)$model)->{$status_attribute ?? 'status'})</span>
+@php
+    $status_attribute ??= 'status';
+    $status = ((object)$model)->{$status_attribute};
 
-@elseif(((object)$model)->{$status_attribute ?? 'status'} == ($status_pending ?? 'pending') || in_array(((object)$model)->{$status_attribute ?? 'status'}, array_merge([], $pending_statuses ?? [])))
-<span class="badge badge-warning">@lang('wncms::word.' . ((object)$model)->{$status_attribute ?? 'status'})</span>
+    $active = array_merge(['active', 'paid', 'success', 'published'], $active_statuses ?? []);
+    $pending = array_merge(['pending'], $pending_statuses ?? []);
+    $completed = array_merge(['completed'], $completed_statuses ?? []);
+    $error = array_merge(['error', 'rejected', 'unread', 'trashed', 'inactive', 'paused'], $error_statuses ?? []);
 
-@elseif(((object)$model)->{$status_attribute ?? 'status'} == ($status_completed ?? 'completed') || in_array(((object)$model)->{$status_attribute ?? 'status'}, array_merge([], $completed_statuses ?? [])))
-<span class="badge badge-success">@lang('wncms::word.' . ((object)$model)->{$status_attribute ?? 'status'})</span>
+    $style = 'secondary';
 
-@elseif(((object)$model)->{$status_attribute ?? 'status'} == ($status_error ?? 'error') || in_array(((object)$model)->{$status_attribute ?? 'status'}, array_merge(['rejected', 'unread', 'trashed'], $error_statuses ?? [])))
-<span class="badge badge-danger">@lang('wncms::word.' . ((object)$model)->{$status_attribute ?? 'status'})</span>
+    if (in_array($status, $active)) {
+        $style = 'success';
+    } elseif (in_array($status, $pending)) {
+        $style = 'warning';
+    } elseif (in_array($status, $completed)) {
+        $style = 'success';
+    } elseif (in_array($status, $error)) {
+        $style = 'danger';
+    }
+@endphp
 
-@else
-<span class="badge badge-secondary">@lang('wncms::word.' . ((object)$model)->{$status_attribute ?? 'status'})</span>
-
-@endif
+<span class="{{ !empty($badgeStyle) ? "badge badge-{$style}" : "text-{$style} fw-bold" }}">
+    @lang('wncms::word.' . $status)
+</span>
