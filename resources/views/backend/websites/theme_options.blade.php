c0b3855f590621b35f5893df5c2aa98aa37a7f8a
secretwebmaster <earnbyshare2016@gmail.com>
2025-11-20 22:08:41 +0800

    feat(theme-options): redesign backend theme options UI with tabs, pickr fixes, modal clone and improved layout

--- resources/views/backend/websites/theme_options.blade.php
+++ resources/views/backend/websites/theme_options.blade.php
@@ -1,133 +1,132 @@
 @extends('wncms::layouts.backend')
 
 @push('head_css')
-<link rel="stylesheet" href="{{ asset('wncms/css/pickr.min.css') }}">
-<style>
-    .nav-link.active{
-        font-weight: bold
-    }
-</style>
+    <link rel="stylesheet" href="{{ asset('wncms/css/pickr.min.css') }}">
+    <style>
+        .nav-link.active {
+            font-weight: bold
+        }
+    </style>
 @endpush
 
 @section('content')
 
-@include('wncms::backend.parts.message')
+    @include('wncms::backend.parts.message')
 
-<div class="row mx-auto mb-3 gx-1">
-    <div class="ms-0 mb-1 mb-md-0 col-12 col-md-auto"><a href="{{ wncms_add_https($_website->domain) }}" target="_blank" class="btn btn-sm btn-info fw-bold text-truncate w-100">@lang('wncms::word.current_website'): {{ $_website->site_name }} ({{ $_website->domain }})</a></div>
-    <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><span class="btn btn-sm btn-danger fw-bold text-truncate w-100">@lang('wncms::word.current_theme'): {{ $_website->theme }}</span></div>
-    <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><a href="{{ route('websites.edit', $_website) }}" class="btn btn-sm btn-primary fw-bold text-truncate w-100">@lang('wncms::word.switch_to_edit_website')</a></div>
-    <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><button type="button" class="btn btn-sm btn-primary fw-bold text-truncate w-100" data-bs-toggle="modal" data-bs-target="#clone_theme_options_from_another_website">@lang('wncms::word.clone_theme_options_from_another_website')</button></div>
-    <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><button type="button" class="btn btn-sm btn-primary fw-bold text-truncate w-100" data-bs-toggle="modal" data-bs-target="#modal_import_default_theme_option">@lang('wncms::word.import_default_theme_option')</button></div>
-</div>
+    <div class="row mx-auto mb-3 gx-1">
+        <div class="ms-0 mb-1 mb-md-0 col-12 col-md-auto"><a href="{{ wncms_add_https($_website->domain) }}" target="_blank" class="btn btn-sm btn-info fw-bold text-truncate w-100">@lang('wncms::word.current_website'): {{ $_website->site_name }} ({{ $_website->domain }})</a></div>
+        <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><span class="btn btn-sm btn-danger fw-bold text-truncate w-100">@lang('wncms::word.current_theme'): {{ $_website->theme }}</span></div>
+        <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><a href="{{ route('websites.edit', $_website) }}" class="btn btn-sm btn-primary fw-bold text-truncate w-100">@lang('wncms::word.switch_to_edit_website')</a></div>
+        <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><button type="button" class="btn btn-sm btn-primary fw-bold text-truncate w-100" data-bs-toggle="modal" data-bs-target="#clone_theme_options_from_another_website">@lang('wncms::word.clone_theme_options_from_another_website')</button></div>
+        <div class="ms-0 ms-md-1 mb-1 mb-md-0 col-6 col-md-auto"><button type="button" class="btn btn-sm btn-primary fw-bold text-truncate w-100" data-bs-toggle="modal" data-bs-target="#modal_import_default_theme_option">@lang('wncms::word.import_default_theme_option')</button></div>
+    </div>
 
-<form class="form" method="POST" action="{{ route('websites.theme.options.update' , $_website) }}" enctype="multipart/form-data">
+    <form class="form" method="POST" action="{{ route('websites.theme.options.update', $_website) }}" enctype="multipart/form-data">
 
-    <div class="card show">
-        @csrf
-        @method('PUT')
-        <div class="card-body border-top px-5 py-3">
+        <div class="card show">
+            @csrf
+            @method('PUT')
+            <div class="card-body border-top px-5 py-3">
 
-            <ul class="nav nav-tabs nav-line-tabs mb-5 fs-6">
-                @foreach($option_tabs ?? [] as $tab_name => $options)
-                    <li class="nav-item">
-                        <a class="nav-link @if($loop->iteration == 1) active @endif" 
-                            data-bs-toggle="tab" href="#tab_{{ $tab_name }}"
-                            data-bs-target="#tab_{{ $tab_name }}"
-                            {{-- >@lang("{$_website->theme}.{$tab_name}")</a> --}}
-                            >{{ wncms()->theme()->translate($_website->theme, $tab_name) }}</a>
-                    </li>
-                @endforeach
-            </ul>
+                <ul class="nav nav-tabs nav-line-tabs mb-5 fs-6">
+                    @foreach ($option_tabs ?? [] as $tab_name => $options)
+                        <li class="nav-item">
+                            <a class="nav-link @if ($loop->iteration == 1) active @endif"
+                                data-bs-toggle="tab" href="#tab_{{ $tab_name }}"
+                                data-bs-target="#tab_{{ $tab_name }}"
+                                {{-- >@lang("{$_website->theme}.{$tab_name}")</a> --}}>{{ wncms()->theme()->translate($_website->theme, $tab_name) }}</a>
+                        </li>
+                    @endforeach
+                </ul>
 
-            <div class="tab-content">
-                @foreach($option_tabs ?? [] as $tab_name => $options)
-                    <div class="tab-pane fade @if($loop->iteration == 1) show active @endif" id="tab_{{ $tab_name }}" role="tabpanel">
-                        @foreach($options as $option_index => $option)
-                            @include('wncms::backend.parts.inputs' , [
-                                'website'=> $_website,
-                                'option_index'=>$option_index,
-                                'option'=>$option,
-                                'current_options'=>$current_options,
-                            ])
-                        @endforeach
+                <div class="tab-content">
+                    @foreach ($option_tabs ?? [] as $tab_name => $options)
+                        <div class="tab-pane fade @if ($loop->iteration == 1) show active @endif" id="tab_{{ $tab_name }}" role="tabpanel">
+                            @foreach ($options as $option_index => $option)
+                                @include('wncms::backend.parts.inputs', [
+                                    'website' => $_website,
+                                    'option_index' => $option_index,
+                                    'option' => $option,
+                                    'current_options' => $current_options,
+                                ])
+                            @endforeach
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-5 text-end">
+            <button type="submit" wncms-btn-loading class="btn btn-primary fw-bold wncms-submit w-100 w-md-auto">
+                @include('wncms::backend.parts.submit', ['label' => __('wncms::word.save_all')])
+            </button>
+        </div>
+    </form>
+
+    {{-- Modal clone theme option --}}
+    <div class="modal fade" tabindex="-1" id="clone_theme_options_from_another_website">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form id="form_clone_theme_options_from_another_website" action="{{ route('websites.theme.clone', $_website) }}" method="POST">
+                    @csrf
+                    <div class="modal-header">
+                        <h3 class="modal-title">@lang('wncms::word.clone_theme_options_from_another_website')</h3>
                     </div>
-                @endforeach
+
+                    <div class="modal-body">
+                        @if ($websites->isNotEmpty())
+                            <select name="from_website_id" id="" class="form-select">
+                                <option value="">@lang('wncms::word.please_select')</option>
+                                @foreach ($websites as $websiteOption)
+                                    <option value="{{ $websiteOption->id }}">{{ $websiteOption->domain }}</option>
+                                @endforeach
+                            </select>
+                        @else
+                            <p>@lang('wncms::word.no_other_websites_using_this_theme')</p>
+                        @endif
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">@lang('wncms::word.close')</button>
+                        <button type="submit" class="btn btn-primary" form-id="form_clone_theme_options_from_another_website">@lang('wncms::word.submit')</button>
+                    </div>
+                </form>
+
             </div>
         </div>
     </div>
 
-    <div class="mt-5 text-end">
-        <button type="submit" wncms-btn-loading class="btn btn-primary fw-bold wncms-submit w-100 w-md-auto">
-            @include('wncms::backend.parts.submit', ['label' => __('wncms::word.save_all')])
-        </button>
-    </div>
-</form>
+    {{-- Modal Import default theme option --}}
+    <div class="modal fade" tabindex="-1" id="modal_import_default_theme_option">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form id="form_import_default_theme_option" action="{{ route('websites.theme.import_default_option', $_website) }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="website_id" value="{{ $_website->id }}">
+                    <div class="modal-header">
+                        <h3 class="modal-title">@lang('wncms::word.import_default_theme_option')</h3>
+                    </div>
 
-{{-- Modal clone theme option --}}
-<div class="modal fade" tabindex="-1" id="clone_theme_options_from_another_website">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form id="form_clone_theme_options_from_another_website" action="{{ route('websites.theme.clone', $_website) }}" method="POST">
-                @csrf
-                <div class="modal-header">
-                    <h3 class="modal-title">@lang('wncms::word.clone_theme_options_from_another_website')</h3>
-                </div>
-    
-                <div class="modal-body">
-                    @if($websites->isNotEmpty())
-                        <select name="from_website_id" id="" class="form-select">
-                            <option value="">@lang('wncms::word.please_select')</option>
-                            @foreach($websites as $websiteOption)
-                            <option value="{{ $websiteOption->id }}">{{ $websiteOption->domain }}</option>
-                            @endforeach
-                        </select>
-                    @else
-                        <p>@lang('wncms::word.no_other_websites_using_this_theme')</p>
-                    @endif
-                </div>
-    
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">@lang('wncms::word.close')</button>
-                    <button type="submit" class="btn btn-primary" form-id="form_clone_theme_options_from_another_website">@lang('wncms::word.submit')</button>
-                </div>
-            </form>
-           
+                    <div class="modal-body">
+                        <div class="alert alert-danger">@lang('wncms::word.importing_default_theme_options_will_override_your_current_options')</div>
+                        <input type="text" class="form-control" name="confirmation" placeholder="@lang('wncms::word.enter_default_to_confirm')">
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">@lang('wncms::word.close')</button>
+                        <button type="submit" class="btn btn-danger fw-bold" form-id="form_import_default_theme_option">@lang('wncms::word.confirm')</button>
+                    </div>
+                </form>
+
+            </div>
         </div>
     </div>
-</div>
-
-{{-- Modal Import default theme option --}}
-<div class="modal fade" tabindex="-1" id="modal_import_default_theme_option">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form id="form_import_default_theme_option" action="{{ route('websites.theme.import_default_option', $_website) }}" method="POST">
-                @csrf
-                <input type="hidden" name="website_id" value="{{ $_website->id }}">
-                <div class="modal-header">
-                    <h3 class="modal-title">@lang('wncms::word.import_default_theme_option')</h3>
-                </div>
-    
-                <div class="modal-body">
-                    <div class="alert alert-danger">@lang('wncms::word.importing_default_theme_options_will_override_your_current_options')</div>
-                    <input type="text" class="form-control" name="confirmation" placeholder="@lang('wncms::word.enter_default_to_confirm')" >
-                </div>
-    
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">@lang('wncms::word.close')</button>
-                    <button type="submit" class="btn btn-danger fw-bold" form-id="form_import_default_theme_option">@lang('wncms::word.confirm')</button>
-                </div>
-            </form>
-           
-        </div>
-    </div>
-</div>
 
 @endsection
 
 @push('foot_js')
     @include('wncms::common.js.tinymce')
-    
+
     <script src="{{ asset('wncms/js/jquery.dragsort.min.js?v=' . wncms_get_version('js')) }}"></script>
     <script>
         window.addEventListener('DOMContentLoaded', (event) => {
@@ -135,7 +134,7 @@
             var activeThemeOptionTabCookieName = 'activeThemeOptionTab' + '{{ $_website->id }}'
 
             // Save the active tab to a cookie when a tab is shown
-            $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+            $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function(e) {
                 var tabName = $(e.target).attr('href').replace('#tab_', '');
                 WNCMS.Cookie.Set(activeThemeOptionTabCookieName, tabName, 7); // Cookie will expire in 365 days
             });
@@ -148,8 +147,8 @@
                 var activeThemeOptionTab = WNCMS.Cookie.Get(activeThemeOptionTabCookieName);
 
                 if (activeThemeOptionTab) {
-                    var triggerEl  = document.querySelector('[data-bs-target="#tab_'+ activeThemeOptionTab +'"]')
-                    var  tabTrigger  = new bootstrap.Tab(triggerEl)
+                    var triggerEl = document.querySelector('[data-bs-target="#tab_' + activeThemeOptionTab + '"]')
+                    var tabTrigger = new bootstrap.Tab(triggerEl)
                     tabTrigger.show()
                 }
             }
@@ -159,11 +158,11 @@
             var input_names = [];
             var current_colors = [];
             var pickrs = [];
-            $(".colorpicker-input").each(function(index , item){
+            $(".colorpicker-input").each(function(index, item) {
 
                 input_names[index] = $(this).attr('data-input');
                 current_colors[index] = $(this).attr('data-current');
-                var input_el = $(this).parent().find('input[name="'+ input_names[index] +'"]');
+                var input_el = $(this).parent().find('input[name="' + input_names[index] + '"]');
 
 
                 var pickr = Pickr.create({
@@ -207,26 +206,24 @@
                             save: false,
                         },
                     },
-                    default:current_colors[index] ? current_colors[index] : "#000000"
+                    default: current_colors[index] ? current_colors[index] : "#000000"
 
                 });
 
-
-
                 pickr[index] = pickr;
-                input_el.on('change',function(){
+                input_el.on('change', function() {
                     pickr.setColor(input_el.val())
                 })
 
-                pickr.on('change', function(color, source, instance){
+                pickr.on('change', function(color, source, instance) {
                     // console.log($(pickr.getRoot().root).find('input[type="text"]'));
                     // console.log(color.toHEXA().toString());
                     // console.log($(pickr).parent().find('input[type="text"]').length)
                     var hex = color.toHEXA().toString();
                     // console.log('input[name="'+ input_names[index] +'"]');
                     // console.log($('input[name="'+ input_names[index] +'"]'));
-                    $('input[name="'+ input_names[index] +'"]').val(hex)
-                }).on('changestop',function(){
+                    $('input[name="' + input_names[index] + '"]').val(hex)
+                }).on('changestop', function() {
                     pickr.applyColor();
                     pickr.hide();
                 });
@@ -237,18 +234,36 @@
     </script>
 
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             // Expand all accordions
-            $('.expand-all-accordion-items').click(function () {
+            $('.expand-all-accordion-items').click(function() {
                 const target = $(this).data('target');
                 $(target).find('.accordion-collapse:not(.show)').addClass('show');
             });
 
             // Collapse all accordions
-            $('.collapse-all-accordion-items').click(function () {
+            $('.collapse-all-accordion-items').click(function() {
                 const target = $(this).data('target');
                 $(target).find('.accordion-collapse.show').removeClass('show');
             });
+        });
+    </script>
+
+    <script>
+        window.addEventListener('DOMContentLoaded', (event) => {
+
+            const hash = window.location.hash;
+            if (hash && document.querySelector('[data-bs-target="' + hash + '"]')) {
+                const triggerEl = document.querySelector('[data-bs-target="' + hash + '"]');
+                const tab = new bootstrap.Tab(triggerEl);
+                tab.show();
+            }
+
+            $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function(e) {
+                const id = $(e.target).attr('data-bs-target');
+                history.replaceState(null, null, id);
+            });
+
         });
     </script>
 @endpush
