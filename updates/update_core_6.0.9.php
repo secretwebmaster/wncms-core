b4136e53f6459507c12e4aad24754ec1962e5a1a
secretwebmaster <earnbyshare2016@gmail.com>
2025-12-04 17:07:16 +0800

    chore: add update scripts for 6.0.8 and 6.0.9

---
+++ updates/update_core_6.0.9.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+$thisVersion = '6.0.9';
+
+// This is a test update to see if version is working
+info("running update_{$thisVersion}.php");
+try {
+    uss('core_version', $thisVersion);
+    info("completed update_{$thisVersion}.php");
+} catch (Exception $e) {
+    info("error when running update_{$thisVersion}.php");
+    info("Error: " . $e->getMessage());
+    return;
+}
