04e16f1c2cb4c4ecf23e0a487396521cfe1277dc
secretwebmaster <earnbyshare2016@gmail.com>
2025-12-08 02:34:38 +0800

    chore: bump core version to 6.1.0

--- config/installer.php
+++ config/installer.php
@@ -12,7 +12,7 @@ return [
     | by looping through the array and run "extension_loaded" on it.
     |
     */
-    'version'=>'6.0.10',
+    'version'=>'6.1.0',
     'core' => [
         'minPhpVersion' => '8.2.0+',
     ],
