91dc59aa59a604688b52c3b5b60551385e7adf60
secretwebmaster <earnbyshare2016@gmail.com>
2025-12-12 23:55:37 +0800

    fix: add core update script for v6.1.1

---
+++ updates/update_core_6.1.1.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+$thisVersion = '6.1.1';
+
+info("running update_{$thisVersion}.php");
+
+try {
+    uss('core_version', $thisVersion);
+    info("completed update_{$thisVersion}.php");
+} catch (\Throwable $e) {
+    info("error when running update_{$thisVersion}.php");
+    info("Error: " . $e->getMessage());
+    return;
+}
