cae91a9d7e15eceaadcd73e49ed52ffe4f83a85a
secretwebmaster <earnbyshare2016@gmail.com>
2025-12-26 23:49:02 +0800

    chore(core): bump version to 6.1.2 and sync installer/update

--- config/installer.php
+++ config/installer.php
@@ -12,7 +12,7 @@ return [
     | by looping through the array and run "extension_loaded" on it.
     |
     */
-    'version'=>'6.1.1',
+    'version'=>'6.1.2',
     'core' => [
         'minPhpVersion' => '8.2.0+',
     ],
