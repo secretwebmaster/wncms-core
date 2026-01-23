57d3e1a27bd8ee9ec4aa9704de27b2580790d1cb
secretwebmaster <earnbyshare2016@gmail.com>
2026-01-24 03:25:33 +0800

    Replace mcamara/laravel-localization with secretwebmaster fork

--- config/installer.php
+++ config/installer.php
@@ -12,7 +12,7 @@ return [
     | by looping through the array and run "extension_loaded" on it.
     |
     */
-    'version'=>'6.1.5',
+    'version'=>'6.1.6',
     'core' => [
         'minPhpVersion' => '8.2.0+',
     ],
