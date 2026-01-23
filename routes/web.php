52c97a638ea31b3ea5b561af459e02b612089a27
secretwebmaster <earnbyshare2016@gmail.com>
2026-01-24 02:21:09 +0800

    Make localization route group conditional by enable_translation

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use Wncms\Http\Controllers\Frontend\PageController;
 
+// Main route group
 Route::group([
-        'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
-    ], function(){
+    'prefix' => gss('enable_translation', true) ? LaravelLocalization::setLocale() : null,
+    'middleware' => gss('enable_translation', true) ? ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'] : [],
+], function () {
 
     // Install
     require __DIR__ . '/install.php';
@@ -20,13 +21,41 @@ Route::group([
 
     // Frontend
     require __DIR__ . '/frontend.php';
-
 });
 
-
-//throw 404 if no route is matched
-//pass $exception to the view
+// Fallback route
 Route::fallback([PageController::class, 'fallback']);
-// Route::fallback(function () {
-//     return view('errors.404');
+
+
+
+// use Illuminate\Support\Facades\Route;
+// use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+// use Wncms\Http\Controllers\Frontend\PageController;
+
+
+// Route::group([
+//         'prefix' => LaravelLocalization::setLocale(),
+//         'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+//     ], function(){
+
+//     // Install
+//     require __DIR__ . '/install.php';
+
+//     // Auth
+//     require __DIR__ . '/auth.php';
+
+//     // Backend
+//     require __DIR__ . '/backend.php';
+
+//     // Frontend
+//     require __DIR__ . '/frontend.php';
+
 // });
+
+
+// //throw 404 if no route is matched
+// //pass $exception to the view
+// Route::fallback([PageController::class, 'fallback']);
+// // Route::fallback(function () {
+// //     return view('errors.404');
+// // });
