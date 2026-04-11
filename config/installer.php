26b56d2c68a78459dcb194dede56e8d84b11df2f
secretwebmaster <earnbyshare2016@gmail.com>
2026-04-11 10:17:26 +0800

    chore(release): prepare 6.3.8 updater and installer version

--- config/installer.php
+++ config/installer.php
@@ -12,7 +12,7 @@ return [
     | by looping through the array and run "extension_loaded" on it.
     |
     */
-    'version'=>'6.3.7',
+    'version'=>'6.3.8',
     'core' => [
         'minPhpVersion' => '8.4.0+',
     ],
